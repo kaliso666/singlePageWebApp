a958536fc5ba5366bea3027dc53f05c1bc975a77
unknown <1825275913@qq.com>
2020-12-04 17:11:36 +0800

    加了几个库

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,23 @@
-const reportWebVitals = onPerfEntry => {
+const reportWebVitals = (onPerfEntry) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+      getCLS((onPerfEntry) => {
+        console.log(`cls:${onPerfEntry.value}`)
+      })
+      getFID((onPerfEntry) => {
+        console.log(`fid:${onPerfEntry.value}`)
+      })
+      getFCP((onPerfEntry) => {
+        console.log(`fcp:${onPerfEntry.value}`)
+      })
+      getLCP((onPerfEntry) => {
+        console.log(`lcp:${onPerfEntry.value}`)
+      })
+      getTTFB((onPerfEntry) => {
+        console.log(`ttfb:${onPerfEntry.value}`)
+      })
+    })
   }
-};
+}
 
-export default reportWebVitals;
+export default reportWebVitals
